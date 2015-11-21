2922200e7d41e1c3797b7cc26d68fe7638f24c50
lei7dover <limhoff@wastebits.com>
2015-11-21 14:41:43 -0500

    working on head formatting & user sign in stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.login_field = :login
   end
+
+  #has_many :gifs
+  #validates_presence_of :login, :password, :message => "missing"
+  #has_many :votes
+
 end
