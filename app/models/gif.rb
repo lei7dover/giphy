1a337a5658e3cbd55006474e4284af78a52f4d98
lei7dover <limhoff@wastebits.com>
2015-11-30 23:25:25 -0500

    file upload need to fix copy link & then switch to postgres

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,14 +1,14 @@
 class Gif < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user
-  validates :has_content
+  validate :has_content
   validates_format_of :url, :with => /\.gif\z/, :message => "must have a .gif extension", :allow_blank => true
   has_many :votes
   attachment :gif_image
 
   def specific_image_url
     if url.blank?
-      gif_image
+      gif_image_url
     else
       url
     end
